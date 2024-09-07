7c61db0041992e67d641ac40d0f0b2e741ba2688
swaggyhustler <gedamsubhash086@gmail.com>
2024-09-07 17:49:49 +0530

    Resolving conflicts from previous merge

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,20 @@
 import Login from './components/Login';
 import Register from './components/Register';
 import OwnerRegistration from './components/OwnerRegistration';
-import Navbar from "./components/Navbar"
+import Navbar from './components/Navbar';
+import Home from './components/Home/index';
 import { ToastContainer } from 'react-toastify';
 import {Routes, Route} from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css'; 
-import Home from './components/Home';
 
 const App = () =>{
   return (
     <>
     <ToastContainer />
-    <Navbar/>
+    <Navbar />
     <Routes>
+      <Route path='/' element={<Login />}/>
+      <Route path='/home' element={<Home />} />
       <Route path='/login' element={<Login />}/>
       <Route path='/registerOwner' element={<OwnerRegistration />} />
       <Route path='/register' element={<Register />}/>
