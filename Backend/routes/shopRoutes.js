dcff2fa0ca8e43472cadd9b4f5af45fb16857f1e
swaggyhustler <gedamsubhash086@gmail.com>
2024-09-08 23:08:46 +0530

    Implement functionality of searching products in nearest locaiton to user

--- Backend/routes/shopRoutes.js
+++ Backend/routes/shopRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { upload } from '../middlewares/multer.js';
-import { addShop, addProduct } from '../controllers/shopController.js';
+import { addShop, addProduct, searchProduct } from '../controllers/shopController.js';
 import { getNearestShops } from '../controllers/distanceController.js';
 
 const router=express.Router();
 
 router.post('/add/shop', upload.single('image'), addShop);
-router.post('/add/product', addProduct);
+router.post('/add/product', upload.single('image'), addProduct);
 router.get('/get/nearestShops', getNearestShops);
+router.get('/search/product', searchProduct);
 
 export default router;
