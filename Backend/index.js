f9dc8874002effcc425ce3e9ce67d3bf0b2a88a9
swaggyhustler <gedamsubhash086@gmail.com>
2024-09-08 15:57:12 +0530

    Modify API Document

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,10 @@ import express from 'express';
 import shopRoutes from './routes/shopRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+
+dotenv.config();
 const app=express();
 
 await connectDB();
@@ -14,6 +17,7 @@ app.use(express.json());
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1', shopRoutes);
 
-app.listen(5000, ()=>{
-    console.log(`Server running on PORT ${5000}`);
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, ()=>{
+    console.log(`Server running on PORT ${PORT}`);
 });
