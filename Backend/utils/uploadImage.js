c76a5adb4ee5e59f040fdb4e64026e5a641b6880
swaggyhustler <gedamsubhash086@gmail.com>
2024-09-08 15:55:29 +0530

    Add functionality of storing images in firebase storage

---
+++ Backend/utils/uploadImage.js
@@ -0,0 +1,18 @@
+import { storage } from "../config/firebase.js";
+import { getDownloadURL, ref, uploadBytes, uploadBytesResumable } from "firebase/storage";
+// import {v4} from "uuid";
+
+const uploadImage = async (file) =>{
+    const dateTime = Date.now();
+    const fileName= `images/${dateTime}`;
+    const storageRef = ref(storage, fileName);
+    const metadata = {
+        contentType: file.mimetype
+    }
+    const snapshot = await uploadBytesResumable(storageRef, file.buffer, metadata);
+    const downloadURL = await getDownloadURL(snapshot.ref);
+    return downloadURL;
+}
+
+export {uploadImage};
+
